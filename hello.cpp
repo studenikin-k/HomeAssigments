e86464e46f399551e75622b88c0cc69b843ddcdb
Kirill Studenikin <st135522@student.spbu.ru>
2024-09-13 17:23:08 +0500

    Localized russian language to main and hello file.

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 std::string hello() {
+    setlocale(LC_ALL, "russian");
     std::string input;
     std::getline(std::cin, input);
     return input;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
-
-
+#include "hello.h"
 int main() {
+    setlocale(LC_ALL, "russian");
     std::cout << "Hello World!\n";
+    while (true) {
+        std::string input = hello();
+        std::cout << "Hello " + input + "!\n";
+    }
+
 }
