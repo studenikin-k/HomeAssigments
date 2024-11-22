8f3fc21af71701c11ec58966b8476349f7a1f441
Kirill studenikin <st135522@spbu.ru>
2024-11-22 16:18:09 +0300

    update gun_type system and constructor

--- Assignment4/gun_type.cpp
+++ Assignment4/gun_type.cpp
@@ -3,42 +3,42 @@
 
 
 Gun_type::Gun_type(std::string gun_type)
-    : _auto_riffle("AK-47"), _sniper_riffle("AWP"), _bazooka("BAZOOKA"), _arm("arm"),
-	_auto_riffle_damage(45), _sniper_riffle_damage(70), _bazooka_damage(100), _arm_damage(50){
+    : _auto_riffle_damage(35), _sniper_riffle_damage(70), _bazooka_damage(100), _arm_damage(50),
+      _ammo_auto_riffle(90), _ammo_sniper_riffle(30), _ammo_bazooka(5), _ammo_arm(100)
+{
       std::cout << "Types of gun was created.\n";
 }
 
-std::string Gun_type::Get_auto_riffle() {
-  return _auto_riffle;
-}
 
-std::string Gun_type::Get_sniper_riffle(){
-	return _sniper_riffle;
-}
+int Gun_type::Get_damage_of_gun(std::string gun_type){
 
-std::string Gun_type::Get_bazooka(){
-  return _bazooka;
-}
-
-std::string Gun_type::Get_arm(){
-  return _arm;
-}
-
-int Gun_type::Get_damage_of_gun(std::string type_of_gun){
-
-  if (type_of_gun == _auto_riffle){
+  if (gun_type == "AKM"){
     return _auto_riffle_damage;
   }
-  else if (type_of_gun == _sniper_riffle){
+  else if (gun_type == "AWP"){
     return _sniper_riffle_damage;
   }
-  else if (type_of_gun == _bazooka){
+  else if (gun_type == "BAZOOKA"){
     return _bazooka_damage;
   }
-  else {
+  else if (gun_type == "arm"){
     return _arm_damage;
   }
 
 }
 
-int Gun_type::Get_ammo(std::string gun_type){}
+int Gun_type::Get_ammo(std::string gun_type){
+   if (gun_type == "AKM"){
+    return _ammo_auto_riffle;
+  }
+  else if (gun_type == "AWP"){
+    return _ammo_sniper_riffle;
+  }
+  else if (gun_type == "BAZOOKA"){
+    return _ammo_bazooka;
+  }
+  else if (gun_type == "arm"){
+    return _ammo_arm;
+  }
+
+}
--- Assignment4/gun_type.h
+++ Assignment4/gun_type.h
@@ -3,26 +3,24 @@
 
 
 
-class Gun_type {
+class Gun_type
+{
    public:
 	Gun_type::Gun_type(std::string);
     virtual ~Gun_type();
-    std::string Get_auto_riffle();
-    std::string Get_sniper_riffle();
-    std::string Get_bazooka();
-    std::string Get_arm();
+
     int Get_damage_of_gun(std::string type_of_gun);
     int Get_ammo(std::string type_of_gun);
+
    protected:
-       std::string _auto_riffle;
-       std::string _sniper_riffle;
-       std::string _bazooka;
-       std::string _arm;
        constexpr int _auto_riffle_damage;
        constexpr int _sniper_riffle_damage;
        constexpr int _bazooka_damage;
        constexpr int _arm_damage;
-
+	   int _ammo_auto_riffle;
+       int _ammo_sniper_riffle;
+       int _ammo_bazooka;
+       int _ammo_arm;
 };
 
 
