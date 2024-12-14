cd2f31140d78f8961754487251d2d394bee0daeb
Kirill studenikin <st135522@spbu.ru>
2024-12-14 14:33:55 +0300

    create positive_checker class

---
+++ Assignment6/positive_checker.cpp
@@ -0,0 +1,18 @@
+#include "positive_checker.h"
+#include <iostream>
+
+bool Positive_checker::bar(int number, std::vector<float> &) {
+    return number > 0;
+}
+
+int Positive_checker::c_i_1() {
+    return 1;
+}
+
+float Positive_checker::c_i_2() {
+    return 1.0f;
+}
+
+void Positive_checker::c_i_3() {
+    std::cout << "Positive_checker c_i_3" << std::endl;
+}
---
+++ Assignment6/positive_checker.h
@@ -0,0 +1,18 @@
+#ifndef POSITIVE_CHECKER_H
+#define POSITIVE_CHECKER_H
+
+#include <vector>
+
+class Positive_checker {
+public:
+    bool bar(int number, std::vector<float> &vec);
+
+    int c_i_1();
+
+    float c_i_2();
+
+    void c_i_3();
+};
+
+
+#endif //POSITIVE_CHECKER_H
